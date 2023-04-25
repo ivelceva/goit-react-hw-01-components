1999f16c4b8e6d59ae555068a5f09c7de2d8bb3c
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-04-25 16:31:54 -0700

    profile

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './Profile.module.css';
+
+export const Profile = ({ username,tag, location, stats, avatar }) => {
+    return (
+      <div className={css.profile}>
+        <div className={css.description}>
+          <img src={avatar} alt="User avatar" className={css.avatar} />
+
+          <p className={css.username}>{username}</p>
+          <p className={css.tag}>@{tag}</p>
+          <p className={css.location}>{location}</p>
+        </div>
+
+        <ul className={css.stats}>
+          <li className={css.list}>
+            <span className={css.label}>Followers</span>
+            <span className={css.quantity}>{stats.followers}</span>
+          </li>
+          <li className={css.list}>
+            <span className={css.label}>Views</span>
+            <span className={css.quantity}>{stats.views}</span>
+          </li>
+          <li className={css.list}>
+            <span className={css.label}>Likes</span>
+            <span className={css.quantity}>{stats.likes}</span>
+          </li>
+        </ul>
+      </div>
+    );   
+}
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.shape ({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }),
+};
