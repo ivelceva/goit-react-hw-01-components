1e790ed5578e0aa379126826719d8f6f494d3bed
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-04-27 11:39:09 -0700

    statistics-fix

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -2,6 +2,17 @@ import React from "react";
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
+function generateRandomColor() {
+  const hexValues = '0123456789ABCDEF';
+  let color = '#';
+
+  for (let i = 0; i < 6; i++) {
+    color += hexValues[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
+
 export const Statistics = ({ title, stats}) => {
     return (
       <section className={css.statistics}>
@@ -9,14 +20,18 @@ export const Statistics = ({ title, stats}) => {
 
         <ul className={css.list}>
           {stats.map(stat => (
-            <li className={css.item} key={stat.id}>
+            <li
+              className={css.item}
+              key={stat.id}
+              style={{ backgroundColor: generateRandomColor() }}
+            >
               <span className={css.label}>{stat.label}</span>
               <span className={css.percentage}>{stat.percentage}%</span>
             </li>
           ))}
         </ul>
       </section>
-    )
+    );
 }
  
 Statistics.propTypes = {
